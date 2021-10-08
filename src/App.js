f52fba73ac320e151d248022874d9289f4de146f
chirayumit001 <chirayumit001@gmail.com>
2021-10-08 21:35:21 +0530

    Did some work on upper body part, added background image, might need some corrections :/

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import LowerBody from './components/LowerBody/lowerbody';
 function App() {
   return (
     <div className="App">
+    {/* Routing karni hai Navbar ke links ki so dekhlena */}
+
+
       <NavBar />
        <UpperBody />
       {/* <Search /> */}
       <LowerBody />
       {/* <ServiceCards /> */}
+
+      
     </div>
   );
 }
--- src/components/UpperBody/upperbody.js
+++ src/components/UpperBody/upperbody.js
@@ -1,15 +1,17 @@
 import React from "react";
+import './upperbody.css'
+import background from '../assets/images/background.png'
 
 export default function UpperBody() {
     let upperbodyStyle = {
-        position: "relative",
-        background:"yellow",
+        margin: '0px',
+        position: "absolute",
         width: "100%",
-        height: '30%'
+        height: '40vh',
 }
     return(
-        <div style={upperbodyStyle}>
-            <h1>UpperBody</h1>
+        <div style={upperbodyStyle} >
+            <img src={background}></img>
         </div>
     )
 }
